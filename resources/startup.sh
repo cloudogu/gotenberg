10f5d9944d71a867c20637772238be1aa47fbc4e
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-12-16 11:55:14 +0100

    Improve gotenberg dogu by DoD (#2)
    Make dogu more stable and fulfill the dogu `definition of done`

---
+++ resources/startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "                                     ./////,                    "
+echo "                                 ./////==//////*                "
+echo "                                ////.  ___   ////.              "
+echo "                         ,**,. ////  ,////A,  */// ,**,.        "
+echo "                    ,/////////////*  */////*  *////////////A    "
+echo "                   ////'        \VA.   '|'   .///'       '///*  "
+echo "                  *///  .*///*,         |         .*//*,   ///* "
+echo "                  (///  (//////)**--_./////_----*//////)   ///) "
+echo "                   V///   '°°°°      (/////)      °°°°'   ////  "
+echo "                    V/////(////////\. '°°°' ./////////(///(/'   "
+echo "                       'V/(/////////////////////////////V'      "
+
+
+
+ETCD_LOG_LEVEL=$(doguctl config "logging/root")
+
+exec gotenberg --log-level="${ETCD_LOG_LEVEL}"
